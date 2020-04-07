3408de0a4e8bfb3bab1eb18567859a219199e2e8
Charlie Tirrell <charlietirrell@MacBook-Pro-5.local>
2020-04-07 16:07:17 -0400

    User and List Schemas

--- models/list.js
+++ models/list.js
@@ -11,5 +11,20 @@ const List = new mongoose.Schema({
   phone: String
 });
 
+// Convert incoming time strings to Date objects
+Section.path('time').set(function(time) {
+  return new Date(`1/15/2020 ${time}`);
+});
+
+// Provide a 12-hour time string as a virtual property
+Section.virtual('time12').get(function() {
+  return this.time.toLocaleTimeString('en-US', {hour: 'numeric', minute: 'numeric'});
+});
+
+// Provide a 24-hour time string as a virtual property
+Section.virtual('time24').get(function() {
+  return this.time.toLocaleTimeString('en-US', {hour12: false});
+});
+
 //Export the module
 module.exports = mongoose.model('List', List);
