9a7a325a0d57858e7ec7e6c288b8ca94616a1231
Charlie Tirrell <charlietirrell@MacBook-Pro-5.local>
2020-04-12 16:58:15 -0400

    list contoller changed to section arrangement

--- controllers/lists.js
+++ controllers/lists.js
@@ -1,24 +1,10 @@
 const List = require('../models/lists');
 
-// GET /lists
+// GET /sections?sort=
 module.exports.index = function(request, response, next) {
-  List.distinct('_id')
-    .then(listIDs => response.redirect(`/lists/${listIDs[0]}`))
+  const order = request.query.sort || 'timePosted'; // Default to sort by course
+
+  List.find().sort()
+    .then(sections => response.render('lists/index', {list: lists}))
     .catch(error => next(error));
-};
-
-// GET /lists/:id
-module.exports.retrieve = function(request, response, next) {
-  const queries = [
-    List.findById(request.params.id),
-    List.distinct('_id')
-  ];
-
-  Promise.all(queries).then(function([list, listIDs]) {
-    if (list) {
-      response.render('lists/index', {list: list, listIDs: listIDs});
-    } else {
-      next(); // No such list
-    }
-  }).catch(error => next(error));
 };
