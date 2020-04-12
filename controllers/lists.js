cc5f6bf5ea4d8591e97f8bbc418c248e76cfc023
Charlie Tirrell <charlietirrell@MacBook-Pro-5.local>
2020-04-12 12:05:21 -0400

    updated list controller

--- controllers/lists.js
+++ controllers/lists.js
@@ -1,3 +1,24 @@
-module.exports.index = function(request, response) {
-  response.send('GET /lists');
+const List = require('../models/list');
+
+// GET /lists
+module.exports.index = function(request, response, next) {
+  List.distinct('_id')
+    .then(listIDs => response.redirect(`/lists/${listIDs[0]}`))
+    .catch(error => next(error));
+};
+
+// GET /lists/:id
+module.exports.retrieve = function(request, response, next) {
+  const queries = [
+    List.findById(request.params.id),
+    List.distinct('_id')
+  ];
+
+  Promise.all(queries).then(function([list, listIDs]) {
+    if (list) {
+      response.render('lists/index', {list: list, listIDs: listIDs});
+    } else {
+      next(); // No such list
+    }
+  }).catch(error => next(error));
 };
