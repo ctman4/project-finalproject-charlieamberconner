5dc60ddc3e3d1334f378278ba1cdc1cbed116523
Amber Lopata <alopata98@gmail.com>
2020-04-13 13:38:12 -0400

    changed driverName

--- models/users.js
+++ models/users.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 //define the schema
 const User = new mongoose.Schema({
   _id: String,
-  driverName: [String],
+  driverName: String,
   location: String,
   available: String,
   phone: Number
