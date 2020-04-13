af89e1fdef081e9253489a96005c2c8835aecd21
Amber Lopata <alopata98@gmail.com>
2020-04-13 08:29:57 -0400

    users index and controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,14 @@
+const User = require('../models/users');
+
+
 module.exports.index = function(request, response) {
   response.send('GET /users');
 };
+
+
+// GET /users
+/*
+module.exports.index = function(request, response, next) {
+
+};
+*/
