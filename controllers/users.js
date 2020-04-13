2978544293e6964b3861c2a867b9c17726ab84a1
connershum <connershum@gmail.com>
2020-04-13 10:04:37 -0400

    retreive

--- controllers/users.js
+++ controllers/users.js
@@ -5,6 +5,9 @@ module.exports.index = function(request, response) {
   response.send('GET /users');
 };
 
+module.exports.retrieve = function(request, response) {
+  response.send(`GET /users/${request.params.id}`);
+};
 
 // GET /users
 /*
