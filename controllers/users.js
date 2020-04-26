e799c673c33c1bec3cef49e156e1aaedfbc76629
connershum <connershum@gmail.com>
2020-04-26 11:34:13 -0400

    /login

--- controllers/users.js
+++ controllers/users.js
@@ -15,3 +15,18 @@ module.exports.retrieve = function(request, response, next) {
     }
   }).catch(error => next(error));
 };
+
+
+
+// POST /login (with a user ID in the request body)
+module.exports.login = function(request, response, next) {
+  User.findById(request.body.id)
+    .then(function(user) {
+      if (user) {
+        request.session.user = user;
+        response.status(200).end();
+      } else {
+        next(); // No such user
+      }
+    }).catch(error => next(error));
+};
