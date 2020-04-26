4b44689a616f7b256f56634c5f527f35b13ceb1a
connershum <connershum@gmail.com>
2020-04-26 11:39:13 -0400

    generate sessions

--- app.js
+++ app.js
@@ -12,25 +12,16 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-/*
-// Enter admin mode and return to the previous page
-app.get('/login', function(request, response) {
-  request.session.admin = true;
-  response.redirect('back');
-});
+// Parse request bodies like query strings
+app.use(express.urlencoded({extended: false}));
 
-// Exit admin mode and return to the previous page
-app.get('/logout', function(request, response) {
-  request.session.admin = false;
-  response.redirect('back');
-});
-
-// Make the mode available in all views
-app.use(function(request, response, next) {
-  response.locals.admin = request.session.admin;
-  next();
-});
-*/
+// Generate a session for each client
+app.use(session({
+  name: 'miniproject', // Name of client cookies
+  secret: 'temporary', // Password for client cookies
+  resave: false, // Recommended setting
+  saveUninitialized: false // Recommended setting
+}));
 
 // Ignore icon requests
 app.get('/favicon.ico', function(request, response) {
