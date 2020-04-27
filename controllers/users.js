dd0838d61e3a3128687ba4fd226f3d072a9d4b03
connershum <connershum@gmail.com>
2020-04-27 12:11:11 -0400

    fix

--- controllers/users.js
+++ controllers/users.js
@@ -2,7 +2,7 @@ const User = require('../models/users');
 
 
 module.exports.index = function(request, response) {
-  response.redirect('/users/charlestirrell99');
+  response.redirect('/users/charlietirrell99');
 };
 
 
