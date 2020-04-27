0b008b9e70b28969560f839708900c546bd376fe
connershum <connershum@gmail.com>
2020-04-27 16:45:35 -0400

    fix

--- controllers/users.js
+++ controllers/users.js
@@ -10,7 +10,7 @@ module.exports.retrieve = function(request, response, next) {
   User.findById(request.params.id)
     .then(function(user) {
     if (user) {
-      response.render('users/index', {user: user};
+      response.render('users/index', {user: user});
     } else {
       next(); // No such user
     }
