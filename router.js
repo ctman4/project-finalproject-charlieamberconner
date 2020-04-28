2c9afa6f6254d28aac25eed6ba0aa27c206fba85
Amber Lopata <alopata98@gmail.com>
2020-04-28 10:20:54 -0400

    trying to create user

--- router.js
+++ router.js
@@ -40,7 +40,7 @@ router.get('/logout', function(request, response) {
   response.redirect('/');
 });
 
-
+// handle user requests
 router.get('/users', users.index);
 
 router.get('/users/:id', users.retrieve);
