0b37756124aa470fd51396bdf6b17515e434da19
Charlie Tirrell <charlietirrell@MacBook-Pro-5.local>
2020-05-08 16:04:59 -0400

    fixed model list title

--- reset.js
+++ reset.js
@@ -22,7 +22,7 @@ const List = new mongoose.Schema({
 //model a collection of Lists
 const lists = [
   new List({
-    title: 'View List',
+    title: 'Charlies List',
     customerID: 'charlietirrell99',
     customerName: 'Charles Tirrell',
     location: 'Marion, MA',
