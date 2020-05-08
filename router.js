07a84a8fe38470b704dacdf9e0682d42f4d390a4
connershum <connershum@gmail.com>
2020-05-07 20:47:21 -0400

    requirer comments

--- router.js
+++ router.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const users = require('./controllers/users');
 const lists = require('./controllers/lists');
+const lists = require('./controllers/comments');
 
 const router = express.Router();
 
