4c9532f2fb461f7a72e691c000499d79d3537cae
connershum <connershum@gmail.com>
2020-05-07 20:50:41 -0400

    fix

--- controllers/comments.js
+++ controllers/comments.js
@@ -2,6 +2,6 @@ const Comment = require('../models/comments');
 
 module.exports.comment = function(request, response, next) {
   Comment.find({ list: request.query.list})
-    .then(lists => response.render('lists/comments', {lists: lists}))
+    .then(lists => response.render('lists/comments', {comments: comments}))
     .catch(error => next(error));
 };
